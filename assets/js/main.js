38e0e034575d6a6c8730aaa621a9ac0071dc73c5
Diego <Petronacidiego@gmail.com>
2020-12-01 16:50:38 +0100

    esercizio Completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,39 +16,37 @@ var numeriCasuali = [];
 var risposteUtente = [];
 var numeriRicordati = [];
 
-
-for (var i = 0; i < 5; i++) {
-    var numeroCasuale = getRandomNumber(1, 100);
-    numeriCasuali.push(numeroCasuale);
-}
-console.log(numeriCasuali);
-
+$("button").click(function () {
+    for (var i = 0; i < 5; i++) {
+        var numeroCasuale = getRandomNumber(1, 100);
+        numeriCasuali.push(numeroCasuale);
+    }
+    console.log(numeriCasuali);
+});
 
 //Dopo 30 secondi l’utente deve inserire, un prompt alla volta
 //un prompt alla volta, i numeri che ha visto precedentemente.
+//il software dice quanti e quali numeri sono stati ricordati.
 
 
 $("button").click(function() {
 
     alert(numeriCasuali);
 
-    for (var i = 0; i < 5; i++) {
-        setTimeout(function () {
-            var rispostaUtente = Number(prompt("inserisci uno dei Numeri"));
-            risposteUtente.push(rispostaUtente);
-            console.log(risposteUtente);
-        }, 1000);
-
-    };
-
-    //il software dice quanti e quali numeri sono stati ricordati.
-     
-    for (var i = 0; i < 5; i++) {
-        if (risposteUtente[i] === numericasuali[i]) {
-            numeriRicordati.push(risposteUtente[i]);
-        };
-    };
     
-});
+        setTimeout(function () {
+            for (var i = 0; i < 5; i++) {
+                var rispostaUtente = Number(prompt("inserisci uno dei Numeri"));
+                risposteUtente.push(rispostaUtente);
+            };
+            console.log(risposteUtente);
 
-console.log(numeriRicordati);
+            for (var i = 0; i < 5; i++) {
+                if (numeriCasuali.includes(risposteUtente[i])) {
+                    numeriRicordati.push(risposteUtente[i]);
+                }
+            };
+            console.log(numeriRicordati);
+            $(".punteggio").text("il tuo punteggio è di " + numeriRicordati.length + " ed i numeri ricordati sono: " + numeriRicordati)
+        }, 30000);    
+});
